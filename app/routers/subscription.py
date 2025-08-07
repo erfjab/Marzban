e05b0632f3bf4c86a51a02a881c84f911e870092
Erfan <143827987+erfjab@users.noreply.github.com>
2025-08-08 01:36:08 +0330

    improve[sub]: change title to username (#8)

--- app/routers/subscription.py
+++ app/routers/subscription.py
@@ -71,7 +71,7 @@ def user_subscription(
         "content-disposition": f'attachment; filename="{user.username}"',
         "profile-web-page-url": str(request.url),
         "support-url": SUB_SUPPORT_URL,
-        "profile-title": encode_title(SUB_PROFILE_TITLE),
+        "profile-title": encode_title(user.username),
         "profile-update-interval": SUB_UPDATE_INTERVAL,
         "subscription-userinfo": "; ".join(
             f"{key}={val}"
